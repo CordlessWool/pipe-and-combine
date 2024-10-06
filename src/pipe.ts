7c2ad80c727f41867a5fcedcbaf63c3200d08336
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-10-07 00:16:14 +0200

    make PipeFunction type general and use AnyFunction

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,20 +1,16 @@
-import type { LastIndex, Prev } from "./helper";
+import type { AnyFunction, LastIndex, Prev } from "./helper";
 
-type PipeFunction<TInput extends Array<any> = Array<any>, TOutput = any> = (
-  ...inputs: TInput
-) => TOutput;
-
-type PipeReduce<AF extends PipeFunction, BF extends PipeFunction> =
-  BF extends PipeFunction<[ReturnType<AF>], unknown>
+type PipeReduce<AF extends AnyFunction, BF extends AnyFunction> =
+  BF extends AnyFunction<[ReturnType<AF>], unknown>
     ? BF
     : (...value: [ReturnType<AF>]) => unknown;
 
-type PipeArray<T extends readonly PipeFunction[]> = {
+type PipeArray<T extends readonly AnyFunction[]> = {
   [X in keyof T]: X extends "0" | 0 ? T[X] : PipeReduce<T[Prev<X>], T[X]>;
 };
 
 export const pipe =
-  <T extends readonly PipeFunction[]>(...fus: PipeArray<T>) =>
+  <T extends readonly AnyFunction[]>(...fus: PipeArray<T>) =>
   (...input: Parameters<T[0]>): ReturnType<PipeArray<T>[LastIndex<T>]> =>
     fus.reduce((acc, f) => f(acc), input) as ReturnType<
       PipeArray<T>[LastIndex<T>]
