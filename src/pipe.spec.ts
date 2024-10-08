2f5c31c9a5e10df45fefdb5699cc162cc8850678
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-10-08 23:40:54 +0200

    build pipe before execution

--- src/pipe.spec.ts
+++ src/pipe.spec.ts
@@ -1,5 +1,5 @@
 import { test, describe, expect } from "vitest";
-import { asyncPipe, pipe } from "./pipe";
+import { asyncPipe, awit, dispel, pipe } from "./pipe";
 
 describe("pipe", () => {
   test("should return 25", () => {
@@ -10,6 +10,32 @@ describe("pipe", () => {
 
     const pipeline = pipe(double, increment, square, toStr);
     expect(pipeline(2)).toBe("25");
+  });
+
+  test("dispel", () => {
+    const double = (x: number) => x * 2;
+    const increment = (x: number) => x + 1;
+    const rest = (x: number): [number, number] => {
+      const b = x % 2;
+      const a = x - b;
+      return [a, b];
+    };
+    const multiply = (x: number, y: number) => x * y;
+    const toStr = (x: number) => x.toString();
+    const pipeline = pipe(double, increment, rest, dispel(multiply), toStr);
+    expect(pipeline(1)).toBe("2");
+  });
+
+  test("awit", () => {
+    const increment = (x: number) => x + 1;
+    const squareSync = (x: number) => x * x;
+    const square = (x: number): Promise<number> =>
+      new Promise((resolve) => setTimeout(() => resolve(x * x), 100));
+    const toStr = async (x: number): Promise<string> =>
+      new Promise((resolve) => setTimeout(() => resolve(x.toString()), 100));
+
+    const pipeline = pipe(increment, awit(square), awit(toStr));
+    expect(pipeline(2)).resolves.toBe("9");
   });
 });
 
--- src/pipe.ts
+++ src/pipe.ts
@@ -1,10 +1,4 @@
-import type {
-  AnyFunction,
-  ArrayMaybePromise,
-  LastIndex,
-  MaybePromise,
-  Prev,
-} from "./helper";
+import type { AnyFunction, ArrayMaybePromise, LastIndex, Prev } from "./helper";
 
 /**
  *   AF: A Function
@@ -63,9 +57,9 @@ export const awit =
     return fu(...args);
   };
 
-export const destruct =
+export const dispel =
   <T extends AnyFunction>(fu: T) =>
-  (...args: Parameters<T>): ReturnType<T> => {
+  (args: Parameters<T>): ReturnType<T> => {
     if (args.length === 1 && Array.isArray(args[0])) {
       return fu(...args[0]);
     }
@@ -74,26 +68,43 @@ export const destruct =
 
 export const preparePipe =
   <TInput extends any[], TOutput>(options?: PreparePipeOptions) =>
-  <T extends readonly AnyFunction[]>(...fus: PipeArray<T, TInput, TOutput>) =>
-  (...input: TInput extends any ? Parameters<T[0]> : TInput) =>
-    fus.reduce((acc, f) => f(acc), input) as TOutput extends any
-      ? ReturnType<PipeArray<T>[LastIndex<T>]>
-      : TOutput;
+  <T extends readonly AnyFunction[]>(...fus: PipeArray<T, TInput, TOutput>) => {
+    const [first, ...rest] = fus;
+    const chain = rest.reduce(
+      (acc, f) =>
+        (...args) =>
+          f(acc(...args)),
+      first,
+    );
+    return chain as (
+      ...input: TInput extends any ? Parameters<T[0]> : TInput
+    ) => TOutput extends any ? ReturnType<PipeArray<T>[LastIndex<T>]> : TOutput;
+  };
 
 export const prepareAsyncPipe =
   <TInput extends any[], TOutput>(options?: PreparePipeOptions) =>
   <T extends readonly AnyFunction[]>(
     ...fus: AsyncPipeArray<T, TInput, TOutput>
-  ) =>
-  async (
-    ...input: TInput extends any ? ArrayMaybePromise<Parameters<T[0]>> : TInput
-  ) =>
-    fus.reduce(
-      (acc, f) => awit(f)(acc),
-      Promise.all(input),
-    ) as TOutput extends any
-      ? Awaited<ReturnType<PipeArray<T>[LastIndex<T>]>>
-      : Awaited<TOutput>;
+  ) => {
+    const [first, ...rest] = fus;
+    const chain = rest.reduce(
+      (chain, f) =>
+        async (...args) => {
+          const data = await chain(...args);
+          return f(data);
+        },
+      first,
+    );
+    return chain as (
+      ...input: TInput extends any
+        ? ArrayMaybePromise<Parameters<T[0]>>
+        : TInput
+    ) => Promise<
+      TOutput extends any
+        ? Awaited<ReturnType<PipeArray<T>[LastIndex<T>]>>
+        : Awaited<TOutput>
+    >;
+  };
 
 export const pipe = preparePipe();
 export const asyncPipe = prepareAsyncPipe();
