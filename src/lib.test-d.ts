84e93b2caf75091a4b28ed8273e310da39c90ad2
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-10-12 15:32:18 +0200

    lib integration tests

---
+++ src/lib.test-d.ts
@@ -0,0 +1,35 @@
+import { test, describe, expectTypeOf } from "vitest";
+import { dispel, preparePipe } from "./pipe";
+import { gate } from "./gate";
+
+describe("type integration", () => {
+  interface User {
+    isAdmin: boolean;
+    isUser: boolean;
+    isGuest: boolean;
+    company?: string;
+  }
+  const isAuthorized =
+    (attr: keyof User, value: string | number | boolean) => (user: User) =>
+      user[attr] === value;
+  test("pipe with gate", () => {
+    const user: User = {
+      isAdmin: true,
+      isUser: false,
+      isGuest: false,
+      company: "Cotton-Coding",
+    };
+
+    const preparedPipe = preparePipe<[User], string>();
+
+    const pipeRunner = preparedPipe(
+      gate(
+        isAuthorized("isAdmin", true),
+        isAuthorized("company", "Cotton-Coding"),
+      ),
+      dispel((user: User) => user.company ?? ""),
+    );
+    expectTypeOf(pipeRunner).parameter(0).toEqualTypeOf<User>();
+    expectTypeOf(pipeRunner(user)).toEqualTypeOf<string>();
+  });
+});
