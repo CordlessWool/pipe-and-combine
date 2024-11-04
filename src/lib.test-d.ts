4f2ebf483b6fd4865a1e31021e3a19a8dd72efa6
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-11-04 20:31:59 +0100

    add type test

--- src/lib.test-d.ts
+++ src/lib.test-d.ts
@@ -12,7 +12,28 @@ describe("type integration", () => {
   const isAuthorized =
     (attr: keyof User, value: string | number | boolean) => (user: User) =>
       user[attr] === value;
-  test("pipe with gate", () => {
+  test("preparedPipe with gate", () => {
+    const user: User = {
+      isAdmin: true,
+      isUser: false,
+      isGuest: false,
+      company: "Cotton-Coding",
+    };
+
+    const preparedPipe = preparePipe<[User], string>();
+
+    const pipeRunner = preparedPipe(
+      gate(
+        isAuthorized("isAdmin", true),
+        isAuthorized("company", "Cotton-Coding"),
+      ),
+      dispel((user: User) => user.company ?? ""),
+    );
+    expectTypeOf(pipeRunner).parameter(0).toEqualTypeOf<User>();
+    expectTypeOf(pipeRunner(user)).toEqualTypeOf<string>();
+  });
+
+  test("preparedPipe with preparedPipe", () => {
     const user: User = {
       isAdmin: true,
       isUser: false,
