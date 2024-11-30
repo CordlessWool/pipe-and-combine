e3432cbe556b8ec1cbea83e7bf4fdad50e35bb4f
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-11-30 13:27:41 +0100

    add function description

--- src/helpers/generics.ts
+++ src/helpers/generics.ts
@@ -1,5 +1,12 @@
 import { AnyObject, GMerge } from "../types";
 
+/**
+ * Generic merge function
+ * This is required to merge objects in a type-safe way with pipes
+ *
+ * @param fu - The function to merge
+ * @returns
+ */
 export const g = <const FI extends AnyObject, FO>(
   fu: (args: FI) => FO,
 ): GMerge<FI, FO> =>
@@ -8,6 +15,12 @@ export const g = <const FI extends AnyObject, FO>(
     return { ...data, ...subset };
   }) as GMerge<FI, FO>;
 
+/**
+ * Adds a timestamp (Date) to the object
+ *
+ * @param tag - The key to use for the timestamp
+ * @returns
+ */
 export const addDate = <FI extends AnyObject, T extends string>(tag: T) =>
   g<FI, { [x in T]: Date }>(
     () =>
