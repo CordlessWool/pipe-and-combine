f3c0cb683cc38a9d086708e0f3d71285f6801f25
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-12-02 23:20:28 +0100

    Fix build (#4)
    * include types.d.ts in dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,29 @@
 import typescript from "rollup-plugin-typescript2";
-import { terser } from "rollup-plugin-terser";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: "src/entry.ts", // Entry point
-  output: [
-    {
-      file: "dist/lib.cjs", // CommonJS output
-      format: "cjs",
-      sourcemap: false,
-    },
-    {
-      file: "dist/lib.js", // ESM output
-      format: "esm",
-      sourcemap: false,
-    },
-  ],
-  plugins: [
-    typescript({ tsconfig: "./tsconfig.json" }), // TypeScript plugin
-    terser(), // Minifies the output (optional)
-  ],
-  external: [], // Specify external dependencies here, if any
-};
+export default [
+  {
+    input: "src/entry.ts", // Entry point
+    output: [
+      {
+        file: "dist/lib.cjs", // CommonJS output
+        format: "cjs",
+        sourcemap: false,
+      },
+      {
+        file: "dist/lib.js", // ESM output
+        format: "esm",
+        sourcemap: false,
+      },
+    ],
+    plugins: [
+      typescript({ tsconfig: "./tsconfig.json" }), // TypeScript plugin
+    ],
+    external: [], // Specify external dependencies here, if any
+  },
+  {
+    input: "src/types.d.ts",
+    output: [{ file: "dist/types.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
