059466d24fa07392c6025b542019cbfdefcd671a
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-01-03 00:14:22 +0100

    export index instaed just helpers

--- src/entry.ts
+++ src/entry.ts
@@ -1,5 +1,5 @@
 export { pipe, preparePipe } from "./pipe";
 export { combine, prepareCombine } from "./combine";
 export { gate, prepareGate } from "./gate";
-export * from "./helpers";
+export * from "./helpers/index";
 export type * from "./types";
